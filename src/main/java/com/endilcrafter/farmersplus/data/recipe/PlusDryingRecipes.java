adde12ee6a6d2e2404858d02eee9a7ee5a32b6f3
shiro <shirokieiyuu@gmail.com>
2024-06-23 09:39:23 +0900

    add recipe

--- src/main/java/com/endilcrafter/farmersplus/data/recipe/PlusDryingRecipes.java
+++ src/main/java/com/endilcrafter/farmersplus/data/recipe/PlusDryingRecipes.java
@@ -11,8 +11,8 @@ import java.util.function.Consumer;
 
 public class PlusDryingRecipes {
     public static void register(Consumer<FinishedRecipe> consumer) {
-        DryingRecipeBuilder.dryingRecipe(Ingredient.of(PlusItems.APPLE_SLICE.get()), PlusItems.DRIED_APPLE_SLICE.get()).build(consumer);
-        DryingRecipeBuilder.dryingRecipe(Ingredient.of(PlusItems.GOLDEN_APPLE_SLICE.get()), PlusItems.DRIED_GOLDEN_APPLE_SLICE.get()).build(consumer);
+        DryingRecipeBuilder.dryingRecipe(Ingredient.of(PlusItems.APPLE_SLICE.get()), PlusItems.DRIED_APPLE_SLICE.get(), 300).build(consumer);
+        DryingRecipeBuilder.dryingRecipe(Ingredient.of(PlusItems.GOLDEN_APPLE_SLICE.get()), PlusItems.DRIED_GOLDEN_APPLE_SLICE.get(), 300).build(consumer);
         DryingRecipeBuilder.dryingRecipe(Ingredient.of(Items.BEEF), PlusItems.DRIED_BEEF.get()).build(consumer);
         DryingRecipeBuilder.dryingRecipe(Ingredient.of(Items.CHICKEN), PlusItems.DRIED_CHICKEN.get()).build(consumer);
         DryingRecipeBuilder.dryingRecipe(Ingredient.of(ModItems.COD_SLICE.get()), PlusItems.DRIED_COD_SLICE.get()).build(consumer);
@@ -21,6 +21,8 @@ public class PlusDryingRecipes {
         DryingRecipeBuilder.dryingRecipe(Ingredient.of(Items.RABBIT), PlusItems.DRIED_RABBIT.get()).build(consumer);
         DryingRecipeBuilder.dryingRecipe(Ingredient.of(ModItems.SALMON_SLICE.get()), PlusItems.DRIED_SALMON_SLICE.get()).build(consumer);
 
+        DryingRecipeBuilder.dryingRecipe(Ingredient.of(Items.KELP), Items.DRIED_KELP, 300).build(consumer);
+        DryingRecipeBuilder.dryingRecipe(Ingredient.of(PlusItems.KELP_BLOCK.get()), Items.DRIED_KELP_BLOCK, 1200).build(consumer);
         DryingRecipeBuilder.dryingRecipe(Ingredient.of(Items.WET_SPONGE), Items.SPONGE).build(consumer);
     }
 }
